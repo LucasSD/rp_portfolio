5614520067ad7da964c33ffdd9688ef76e185b82
LucasSD <lucas.stonedrake@gmail.com>
2021-03-26 15:36:13 +0000

    add blog app and project instances via admin

--- projects/models.py
+++ projects/models.py
@@ -3,6 +3,12 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
+    summary = models.CharField(max_length=100)
     technology = models.CharField(max_length=20)
-    image = models.FilePathField(path="/img")
+    #image = models.ImageField()
+    image = models.FilePathField(path=r"C:\Users\Haamiyah\Desktop\Lucas\Python Stuff\rp_portfolio\projects\static\img")
+    repo = models.URLField()
+
+    def __str__(self):
+        return self.title #or self.title for blog posts
 
