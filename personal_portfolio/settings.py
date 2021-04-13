1232b8046d50681695a23883d5a71f6be757259a
LucasSD <lucas.stonedrake@gmail.com>
2021-04-13 16:15:29 +0100

    change TEMPLATE DIRS in settings.py

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'personal_portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["personal_portfolio/templates/"],
+        'DIRS': ["rp_portfolio/personal_portfolio/templates/"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
