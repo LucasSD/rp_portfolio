bd49be98d1882425a2ea02ed54cf51a29983a1c3
LucasSD <lucas.stonedrake@gmail.com>
2021-04-14 12:18:00 +0100

    set static_root in settings.py

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -118,4 +118,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = "/home/lucasstonedrake/rp_portfolio/static"
 STATIC_URL = '/static/'
