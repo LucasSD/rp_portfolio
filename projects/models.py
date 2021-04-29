016fcf572cb4575a9c47e8afb2fe37eca37cb0e7
LucasSD <lucas.stonedrake@gmail.com>
2021-04-29 16:46:39 +0100

    Reformat eight files with black and remove redundant comments

--- projects/models.py
+++ projects/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 
+
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     summary = models.CharField(max_length=100)
     technology = models.CharField(max_length=20)
-    #image = models.ImageField()
-    #image = models.FilePathField(path="rp_portfolio/static/img")
     image = models.CharField(max_length=100)
     repo = models.URLField()
 
     def __str__(self):
-        return self.title #or self.title for blog posts
+        return self.title
--- projects/views.py
+++ projects/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 from projects.models import Project
 
+
 def project_index(request):
     projects = Project.objects.all()
     context = {
-        'projects': projects
-    } # always need a context dict when retrieving from a db
-    return render(request, 'project_index.html', context)
+        "projects": projects
+    }  # always need a context dict when retrieving from a db
+    return render(request, "project_index.html", context)
+
 
 def project_detail(request, pk):
     project = Project.objects.get(pk=pk)
-    context = {
-        'project': project
-    }
-    return render(request, 'project_detail.html', context)
+    context = {"project": project}
+    return render(request, "project_detail.html", context)
